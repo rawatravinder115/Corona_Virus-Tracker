86f7ab0fbcc342c62be32c241124baa7253e45be
rawatravinder115 <rawatravinder115@gmail.com>
2021-04-07 19:45:48 +0530

    different from previous day

--- src/main/java/ProjectCoronaVirusTracker/Corona_VirusTracker/Controllers/HomeController.java
+++ src/main/java/ProjectCoronaVirusTracker/Corona_VirusTracker/Controllers/HomeController.java
@@ -1,11 +1,14 @@
 package ProjectCoronaVirusTracker.Corona_VirusTracker.Controllers;
 
 import ProjectCoronaVirusTracker.Corona_VirusTracker.CoronaVirusTrackerApplication;
+import ProjectCoronaVirusTracker.Corona_VirusTracker.Models.LocationStats;
 import ProjectCoronaVirusTracker.Corona_VirusTracker.Services.CoronaVirusDataServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -15,7 +18,11 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model){
-        model.addAttribute("locationStats",coronaVirusDataServices.getAllStats());
+        List<LocationStats> allstats = coronaVirusDataServices.getAllStats();
+        int totalReportedCases =allstats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        model.addAttribute("locationStats",allstats);
+        model.addAttribute("totalReportedCases",totalReportedCases);
+
         return "home";
     }
 }
